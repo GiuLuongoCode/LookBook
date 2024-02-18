8f105dba4625fc6e27fc161ed1f64a32ab81e42b
GiuLuongoCode <giuseppe.luongo.dev@gmail.com>
2024-02-18 12:52:17 +0100

    Added dotenv

--- src/models/config/database.js
+++ src/models/config/database.js
@@ -1,37 +1,48 @@
 const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const uriStart = process.env.MONGODB_URI_START;
+const uriEnd = process.env.MONGODB_URI_END;
+const password = process.env.MONGODB_PASSWORD;
+const user = process.env.MONGODB_USER;
+const cluster = process.env.MONGODB_CLUSTER;
+const connectionString = `${uriStart}${user}:${password}@${cluster}${uriEnd}`;
 
 class Database {
-    constructor() {
+  constructor() {
+    this.isConnected = false;
+    this.connect();
+  }
+
+  connect() {
+    mongoose
+      .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        this.isConnected = true;
+        console.log("MongoDB connected");
+      })
+      .catch((err) => {
         this.isConnected = false;
-        this.connect();
-    }
+        console.error("MongoDB connection error:", err);
+      });
+  }
 
-    connect() {
-        mongoose.connect('mongodb://localhost:27017/mydatabase', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        .then(() => {
-            this.isConnected = true;
-            console.log('MongoDB connected');
-        })
-        .catch(err => {
-            this.isConnected = false;
-            console.error('MongoDB connection error:', err);
-        });
-    }
+  isConnected() {
+    return this.isConnected;
+  }
 
-    isConnected() {
-        return this.isConnected;
+  async disconnect() {
+    if (this.isConnected) {
+      await mongoose.disconnect();
+      this.isConnected = false;
+      console.log("MongoDB disconnected");
     }
-
-    async disconnect() {
-        if (this.isConnected) {
-            await mongoose.disconnect();
-            this.isConnected = false;
-            console.log('MongoDB disconnected');
-        }
-    }
+  }
 }
 
 module.exports = new Database();
